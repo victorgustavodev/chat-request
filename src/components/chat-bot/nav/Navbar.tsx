85ac68ba254d3d5867e35dc43b85fe22128f2832
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-08-11 21:03:46 -0300

    Feat: retira nav bar da dashboard

--- src/components/chat-bot/nav/Navbar.tsx
+++ src/components/chat-bot/nav/Navbar.tsx
@@ -1,22 +1,21 @@
-import Image from "next/image";
 import { IoMenu } from "react-icons/io5";
 
 export function Navbar(props: { function: () => void }) {
-    return (
-        <div className="w-full">
-                      <div className="flex justify-between items-center p-8">
-                        <button onClick={props.function} className="cursor-pointer text-3xl">
-                          <IoMenu />
-                        </button>
-                        <span>
+  return (
+    <div className="w-full">
+      <div className="flex justify-between items-center p-8">
+        <button onClick={props.function} className="cursor-pointer text-3xl">
+          <IoMenu />
+        </button>
+        {/* <span>
                           <Image
                             src="/images/user-profile.png"
                             width={40}
                             height={40}
                             alt="Picture of the author"
                           />
-                        </span>
-                      </div>
-                    </div>
-    )
+                        </span> */}
+      </div>
+    </div>
+  );
 }
